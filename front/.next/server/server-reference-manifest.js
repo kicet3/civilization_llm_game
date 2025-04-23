561c8a09534b51c73268cec8f9d876a6e8acdb86
kicet3 <kicet3@gmail.com>
2025-04-23 11:32:52 +0900

    init

--- front/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"xfZQKlKEY1FnXNqio/70tuEP0/v6AZMUt0a9peKom0A=\"\n}"
--- front/.next/static/chunks/src_app_game_select-mode_page_tsx_4de27295._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_07528441._.js",
-  "static/chunks/src_baa727f7._.js"
-],
-    source: "dynamic"
-});
